48a3510a80dcf1993d721ed8447c2957706eec93
Eric Updegraff <eugraff2@gmail.com>
2022-10-26 20:25:47 -0400

    Quiz and Question POJO finished

--- app/src/main/java/edu/uga/cs/capitalquiz/Question.java
+++ app/src/main/java/edu/uga/cs/capitalquiz/Question.java
@@ -6,11 +6,34 @@ package edu.uga.cs.capitalquiz;
 
 public class Question {
 
-    private int id;
+    private long id;
     private String name;
     private String capital;
     private String additional1;
     private String additional2;
+
+    public Question() {
+        this.id = -1;
+        this.name = null;
+        this.capital = null;
+        this.additional1 = null;
+        this.additional2 = null;
+    }
+
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String inName) {this.name = inName;}
+
+    public String capital() {return capital;}
+    public void setCapital(String inCapital) {this.capital = inCapital;}
+
+    public String getAdditional1(){return additional1;}
+    public void setAdditional1(String inAdd) {this.additional1 = inAdd;}
+
+    public String getAdditional2(){return additional2;}
+    public void setAdditional2(String inAdd) {this.additional2 = inAdd;}
 
 
 
--- app/src/main/java/edu/uga/cs/capitalquiz/Quiz.java
+++ app/src/main/java/edu/uga/cs/capitalquiz/Quiz.java
@@ -6,7 +6,7 @@ package edu.uga.cs.capitalquiz;
 
 public class Quiz {
 
-    private int id;
+    private long id;
     private String date;
     private double result;
     private int numAnswered;
@@ -17,6 +17,48 @@ public class Quiz {
     private Question q5;
     private Question q6;
 
+    public Quiz() {
+        this.id = -1;
+        this.date = null;
+        this.result = -1.0;
+        this.numAnswered = -1;
+        this.q1 = null;
+        this.q2 = null;
+        this.q3 = null;
+        this.q4 = null;
+        this.q5 = null;
+        this.q6 = null;
+    }
+
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+
+    public String getDate() {return date;}
+    public void setDate(String inDate) {this.date = inDate;}
+
+    public double getResult() {return result;}
+    public void setResult(double inResult) {this.result = inResult;}
+
+    public int getNumAnswered() {return numAnswered;}
+    public void setNumAnswered(int inNum) {this.numAnswered = inNum;}
+
+    public Question getQ1() {return this.q1;}
+    public void setQ1(Question q) {this.q1 = q;}
+
+    public Question getQ2() {return this.q2;}
+    public void setQ2(Question q) {this.q2 = q;}
+
+    public Question getQ3() {return this.q3;}
+    public void setQ3(Question q) {this.q3 = q;}
+
+    public Question getQ4() {return this.q4;}
+    public void setQ4(Question q) {this.q4 = q;}
+
+    public Question getQ5() {return this.q5;}
+    public void setQ5(Question q) {this.q5 = q;}
+
+    public Question getQ6() {return this.q6;}
+    public void setQ6(Question q) {this.q6 = q;}
 
 
 } // Quiz
