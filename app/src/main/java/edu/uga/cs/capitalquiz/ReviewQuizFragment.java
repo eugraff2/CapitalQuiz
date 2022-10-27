22442e138ccdb4fa5a8e273b761d13e303acb4a0
Eric Updegraff <eugraff2@gmail.com>
2022-10-26 20:52:50 -0400

    QuizFragment and state_capitals.csv files added to project

---
+++ app/src/main/java/edu/uga/cs/capitalquiz/ReviewQuizFragment.java
@@ -0,0 +1,13 @@
+package edu.uga.cs.capitalquiz;
+
+import androidx.fragment.app.Fragment;
+
+public class ReviewQuizFragment extends Fragment {
+
+    private static final String TAG = "ReviewQuizFragment";
+
+
+
+
+} // ReviewQuizFragment
+
